8d92be22ce24cbffc01a1385848e4bde89aa0865
“claudio-code” <suporte2424@outlook.com>
2024-03-08 22:19:02 -0300

    Fixed bugs to openJdk 8

--- install_certs.sh
+++ install_certs.sh
@@ -3,47 +3,65 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-println () {
-    local message="$1"
-    echo -e "${GREEN}[INFO]${NC} - ${message}"
+JAVA_VENDOR=$(java -version 2>&1 | head -n 3 | awk -F 'Runtime' '{print $2}' | tr '\n' ' ')
+JAVA_VENDOR_OUT=($JAVA_VENDOR)
+JAVA_ONLY_VERSION=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
+JAVA_ONLY_VERSION_OUT=($JAVA_ONLY_VERSION)
+
+println() {
+	local message="$1"
+	echo -e "${GREEN}[INFO]${NC} - ${message}"
 }
 
-transform_crt_to_der () {
-    local fileName=$(basename -- "$1")
-    local fileNameWithoutExtension="${fileName%.*}"
-    println "Converter $fileName to .der extensions."
-    
-    openssl x509 -in "$fileName" -outform der -out "$fileNameWithoutExtension.der"
+transform_crt_to_der() {
+	local fileName=$(basename -- "$1")
+	local fileNameWithoutExtension="${fileName%.*}"
+	println "Converter $fileName to .der extensions."
+
+	openssl x509 -in "$fileName" -outform der -out "$fileNameWithoutExtension.der"
 }
 
-import_keys_to_java_cacerts () {
-    local fileName=$(basename -- "$1")
-    local fileNameWithoutExtension="${fileName%.*}"
-    println "Import it key $fileName to java path: $javaPath ."
-    
-    keytool -importcert -trustcacerts -alias "$fileNameWithoutExtension" -keystore "$javaPath" -storepass changeit -file "./$fileName"
+import_keys_to_java_cacerts() {
+	local fileName=$(basename -- "$1")
+	local fileNameWithoutExtension="${fileName%.*}"
+	println "Import it key $fileName to java path: $JAVA_HOME ."
+
+	if [[ "${JAVA_ONLY_VERSION_OUT[*]}" =~ '1.8.' ]]; then
+		if [[ "${JAVA_VENDOR_OUT[*]}" =~ 'LTS' || "${JAVA_VENDOR_OUT[*]}" =~ 'Correto' || "${JAVA_VENDOR_OUT[*]}" =~ 'Termurin' ]]; then
+			sudo $JAVA_HOME/bin/keytool -importcert -file "./$fileName" -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -alias "$fileNameWithoutExtension" -noprompt
+		else
+			sudo $JAVA_HOME/bin/keytool -importcert -file "./$fileName" -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -alias "$fileNameWithoutExtension" -noprompt
+		fi
+	else
+		sudo $JAVA_HOME/bin/keytool -importcert -file "./$fileName" -cacerts -storepass changeit -alias "$fileNameWithoutExtension" -noprompt
+	fi
 }
 
-load_der_files () {
-    for file in ./*.der; do
-        import_keys_to_java_cacerts "$file"
-    done    
+load_der_files() {
+	for file in ./*.der; do
+		if [ -d "$file" ]; then
+			for dir in "$file/*"; do
+				for fileSubDir in "$dir"; do
+					import_keys_to_java_cacerts "$fileSubDir"
+				done
+			done
+		else
+			import_keys_to_java_cacerts "$file"
+		fi
+	done
 }
 
-load_crt_files () {
-    for file in ./*.crt; do
-        transform_crt_to_der "$file"
-    done
+load_crt_files() {
+	for file in ./*.crt; do
+		transform_crt_to_der "$file"
+	done
 }
 
-while getopts j:k: flag
-do
-    case "${flag}" in
-        j) javaPath=${OPTARG};;
-        k) keyPath=${OPTARG};;
-    esac
+while getopts j:k: flag; do
+	case "${flag}" in
+	k) keyPath=${OPTARG} ;;
+	esac
 done
-
 
 println "Import all keys to java path."
 cd $keyPath
